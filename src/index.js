162fc7f52da624720fe1fc6705a5145e19d24987
Nnabugwu Chidozie Gabriel <nnabugwuchidozie@gmail.com>
2019-06-24 21:17:57 +0100

    bug(cross-origin):fix cross origin error
    Api docs' request to production server produces cross-origin error.
    Add cors middleware to api-docs route.
    
    [Start #166757788] [Finishes #166757788]

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '@babel/polyfill';
 import express, { json, urlencoded } from 'express';
 import logger from 'morgan';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import routesV1 from './routes/v1';
 import ResultHandler from './helpers/ResultHandler';
 import config from './config';
@@ -12,28 +13,21 @@ const app = express();
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-app.use('/api/v1', routesV1);
-
 app.get('/', (req, res) => res.json({
   message: "Welcome to Auto-Mart. Access API docs through '/api-docs'",
 }));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc), cors);
 app.get('/api', (req, res) => res.json({
   message: "Welcome to Auto-Mart API. Access API docs through '/api-docs'",
 }));
-
+app.use('/api/v1', routesV1);
 app.use('*', (req, res, next) => {
   const error = Error('Not Found');
   error.status = 404;
   next(error);
 });
-
 app.use(ResultHandler.error);
-
 app.set('port', config.PORT);
-
 app.listen(config.PORT, () => {
   console.log('Server started on port: ', config.PORT);
 });
